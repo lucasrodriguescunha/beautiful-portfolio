5067d64e72fc079593736a5b73ec08f7ee7ebb26
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-04-26 20:38:15 -0300

    feat: integrando a funcionalidade EmailJS

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,6 +1,33 @@
-import { RevealOnScroll } from "../RevealOnScroll"
+import { useState } from "react";
+import { RevealOnScroll } from "../RevealOnScroll";
+import emailjs from 'emailjs-com';
 
 export const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        emailjs.
+            sendForm(
+                import.meta.env.VITE_SERVICE_ID,
+                import.meta.env.VITE_TEMPLATE_ID,
+                e.target,
+                import.meta.env.VITE_PUBLIC_KEY
+            )
+            .then((result) => {
+                alert("Message Sent!");
+                setFormData({ name: "", email: "", message: "" })
+            })
+            .catch(() => {
+                alert("Oops! Something went wrong. Please try again.")
+            });
+    };
+
     return (
         <section
             id="contact"
@@ -11,15 +38,17 @@ export const Contact = () => {
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                         Get In Touch
                     </h2>
-                    <form className="space-y-6">
+                    <form className="space-y-6" onSubmit={handleSubmit}>
                         <div className="relative">
                             <input
                                 type="text"
                                 id="name"
                                 name="name"
                                 required
+                                value={formData.name}
                                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                                 placeholder="Name..."
+                                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
                             />
                         </div>
 
@@ -29,8 +58,10 @@ export const Contact = () => {
                                 id="email"
                                 name="email"
                                 required
+                                value={formData.email}
                                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                                 placeholder="example@gmail.com"
+                                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                             />
                         </div>
 
@@ -39,9 +70,11 @@ export const Contact = () => {
                                 id="message"
                                 name="message"
                                 required
+                                value={formData.message}
                                 rows={5}
                                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                                 placeholder="Your Message..."
+                                onChange={(e) => setFormData({ ...formData, message: e.target.value })}
                             />
                         </div>
 
