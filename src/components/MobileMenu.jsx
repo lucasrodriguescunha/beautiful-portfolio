6bb4d5a6beccc77e2ac101555dde47b0fca05784
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-05-29 15:49:58 -0300

    feat: adds the Moodflix project

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -65,6 +65,25 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
             >
                 Contact
             </a>
+
+
+            
+
+            {/*
+            
+            =====> Fazer 
+
+            <a
+                href="#change-language"
+                onClick={() => setMenuOpen(false)}
+                className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+                    ${menuOpen
+                        ? "opacity-100 translate-y-0"
+                        : "opacity-0 translate-y-5"}
+                    `}
+            >
+                Contact
+            </a> */}
         </div>
     );
 }
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -65,14 +65,14 @@ export const Projects = () => {
                             </div>
                         </div>
 
-                        {/* <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-                            <h3 className="text-xl font-bold mb-2"> Cloud Platform </h3>
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                            <h3 className="text-xl font-bold mb-2"> Moodflix </h3>
                             <p className="text-gray-400 mb-4">
                                 Scalable cloud infrastructure managment with real-time monitoring
                                 and automated scaling.
                             </p>
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                                {["Vite", "React.js", "TMDB API", "Appwrite", "Vercel"].map((tech, key) => (
                                     <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
                                 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
                                     >
@@ -83,13 +83,14 @@ export const Projects = () => {
 
                             <div className="flex justify-between items-center">
                                 <a
-                                    href="#"
+                                    href="https://moodflix-steel.vercel.app/"
+                                    target="_blank"
                                     className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                                 >
                                     View Project &#8594;
                                 </a>
                             </div>
-                        </div> */}
+                        </div>
 
                         {/* <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                             <h3 className="text-xl font-bold mb-2"> Cloud Platform </h3>
