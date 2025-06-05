94e8e82f9cfa01bc7fe6e8b6fbba959b96373f33
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-06-05 11:04:58 -0300

    feat: integrated Sentry for error monitoring and analysis

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import * as Sentry from "@sentry/react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import './i18n.js';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+Sentry.init({
+    dsn: "https://3771a8a2226b666585ff88e749ec987a@o4509408313475072.ingest.us.sentry.io/4509446265241600",
+    sendDefaultPii: true,
+});
+
+const container = document.getElementById("root"); // ou "app", depende do seu index.html
+const root = createRoot(container);
+
+root.render(
+    <StrictMode>
+        <Sentry.ErrorBoundary fallback={<p>Algo deu errado.</p>}>
+            <App />
+        </Sentry.ErrorBoundary>
+    </StrictMode>
+);
