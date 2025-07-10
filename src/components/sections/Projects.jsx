d0c93a516db2e9f3d9bcd1fe0c7fdcf757218111
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-07-10 13:01:53 -0300

    feat: add Clear Email project and update project details

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -19,6 +19,32 @@ export const Projects = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                         <div
                             className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                            <h3 className="text-xl font-bold mb-2"> {t('clear_email_title')} </h3>
+                            <p className="text-gray-400 mb-4">
+                                {t('clear_email_description')}
+                            </p>
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {["Python", "imaplib"].map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <a
+                                    href="https://github.com/lucasrodriguescunha/clear-email"
+                                    target="_blank"
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                >
+                                    {t('view_project')} &#8594;
+                                </a>
+                            </div>
+                        </div>
+
+                        <div
+                            className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                             <h3 className="text-xl font-bold mb-2"> {t('document_manager_title')} </h3>
                             <p className="text-gray-400 mb-4">
                                 {t('document_manager_description')}
@@ -97,32 +123,6 @@ export const Projects = () => {
                             </div>
                         </div>
 
-                        <div
-                            className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-                            <h3 className="text-xl font-bold mb-2"> {t('converso_title')} </h3>
-                            <p className="text-gray-400 mb-4">
-                                {t('converso_description')}
-                            </p>
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {["Next.js", "Tailwind CSS", "Shadcn", "Zod", "Supabase", "Clerk", "Sentry", "Vapi AI", "Stripe", "Radix UI", "React Hook Form", "Lottie", "Lucide Icons", "Vercel"].map((tech, key) => (
-                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
-                                    >
-                                        {tech}
-                                    </span>
-                                ))}
-                            </div>
-
-                            <div className="flex justify-between items-center">
-                                <a
-                                    href="https://saas-app-tan.vercel.app/"
-                                    target="_blank"
-                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                                >
-                                    {t('view_project')} &#8594;
-                                </a>
-                            </div>
-                        </div>
 
                         {/* <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                             <h3 className="text-xl font-bold mb-2"> Cloud Platform </h3>
