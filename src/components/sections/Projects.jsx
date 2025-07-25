a1cdf219cde4c4781c9255eca0e45ec85ab9d71e
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-07-25 18:14:35 -0300

    feat: update project details and links in Projects component

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,8 +1,8 @@
-import {RevealOnScroll} from "../RevealOnScroll"
-import {useTranslation} from "react-i18next";
+import { RevealOnScroll } from "../RevealOnScroll"
+import { useTranslation } from "react-i18next";
 
 export const Projects = () => {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     return (
         <section
@@ -42,6 +42,34 @@ export const Projects = () => {
                                 </a>
                             </div>
                         </div>
+
+                        <div
+                            className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                            <h3 className="text-xl font-bold mb-2"> {t('moodflix_title')} </h3>
+                            <p className="text-gray-400 mb-4">
+                                {t('moodflix_description')}
+                            </p>
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {["Vite", "React.js", "TMDB API", "Appwrite", "Vercel"].map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+
+                            <div className="flex justify-between items-center">
+                                <a
+                                    href="https://moodflix-steel.vercel.app/"
+                                    target="_blank"
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                                >
+                                    {t('view_project')} &#8594;
+                                </a>
+                            </div>
+                        </div>
+
 
                         <div
                             className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
@@ -96,32 +124,6 @@ export const Projects = () => {
                             </div>
                         </div>
 
-                        <div
-                            className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-                            <h3 className="text-xl font-bold mb-2"> {t('moodflix_title')} </h3>
-                            <p className="text-gray-400 mb-4">
-                                {t('moodflix_description')}
-                            </p>
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {["Vite", "React.js", "TMDB API", "Appwrite", "Vercel"].map((tech, key) => (
-                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
-                                    >
-                                        {tech}
-                                    </span>
-                                ))}
-                            </div>
-
-                            <div className="flex justify-between items-center">
-                                <a
-                                    href="https://moodflix-steel.vercel.app/"
-                                    target="_blank"
-                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                                >
-                                    {t('view_project')} &#8594;
-                                </a>
-                            </div>
-                        </div>
 
 
                         {/* <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
