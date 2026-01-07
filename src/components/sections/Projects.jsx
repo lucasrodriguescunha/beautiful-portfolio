5851cba43c6bdc6fd770087c05115758484d6806
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-01-06 21:38:17 -0300

    refactor: remove QualiAI project details and update translation keys in Projects component

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -97,35 +97,6 @@ export const Projects = () => {
                             </div>
                         </div>
 
-                        <div
-                            className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-                            <h3 className="text-xl font-bold mb-2"> {t('qualiai_title')} </h3>
-                            <p className="text-gray-400 mb-4">
-                                {t('qualiai_description')}
-                            </p>
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {["Python", "Tensorflow", "Keras", "NumPy", "Matplotlib", "Flask", "MongoDB", "Spring Boot", "React.js", "Docker", "PostgreSQL"].map((tech, key) => (
-                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
-                                    >
-                                        {tech}
-                                    </span>
-                                ))}
-                            </div>
-
-                            <div className="flex justify-between items-center">
-                                <a
-                                    href="https://github.com/lucasrodriguescunha/QualiAI"
-                                    target="_blank"
-                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                                >
-                                    {t('view_project')} &#8594;
-                                </a>
-                            </div>
-                        </div>
-
-
-
                         {/* <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                             <h3 className="text-xl font-bold mb-2"> Cloud Platform </h3>
                             <p className="text-gray-400 mb-4">
