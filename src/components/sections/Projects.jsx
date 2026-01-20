9a4a5d97b455283eebba5870c53c09c27f911dcb
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2026-01-20 13:19:14 -0300

    refactor: remove Memory Game project details and update translations keys in Projects component

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -52,6 +52,44 @@ export const Projects = () => {
                 <a
                   href='#'
                   className='text-blue-400 hover:text-blue-300 transition-colors my-4'
+                  disabled={true}
+                >
+                  Este projeto é totalmente privado
+                </a>
+              </div>
+            </div>
+
+            <div
+              className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all'>
+              <h3 className='text-xl font-bold mb-2'>
+                {t('cryptocurrency_tracker')}
+              </h3>
+              <p className='text-gray-400 mb-4'>
+                {t('cryptocurrency_tracker')}
+              </p>
+              <div className='flex flex-wrap gap-2 mb-4'>
+                {
+                  [
+                    'HTML',
+                    'CSS',
+                    'JavaScript',
+                    'React',
+                    'Recharts',
+                    'Coingecko API'
+                  ].map((tech, key) => (
+                    <span
+                      key={key}
+                      className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition'
+                    >
+                      {tech}
+                    </span>
+                  ))}
+              </div>
+
+              <div className='flex justify-between items-center'>
+                <a
+                  href='#'
+                  className='text-blue-400 hover:text-blue-300 transition-colors my-4'
                 >
                   {t('view_project')} &#8594;
                 </a>
@@ -99,41 +137,6 @@ export const Projects = () => {
             <div
               className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all'>
               <h3 className='text-xl font-bold mb-2'>
-                {t('memory_game_title')}
-              </h3>
-              <p className='text-gray-400 mb-4'>
-                {t('memory_game_description')}
-              </p>
-              <div className='flex flex-wrap gap-2 mb-4'>
-                {
-                  [
-                    'HTML',
-                    'CSS',
-                    'JavaScript',
-                    'React'
-                  ].map((tech, key) => (
-                    <span
-                      key={key}
-                      className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition'
-                    >
-                      {tech}
-                    </span>
-                  ))}
-              </div>
-
-              <div className='flex justify-between items-center'>
-                <a
-                  href='https://memory-game-eosin-two.vercel.app/'
-                  className='text-blue-400 hover:text-blue-300 transition-colors my-4'
-                >
-                  {t('view_project')} &#8594;
-                </a>
-              </div>
-            </div>
-
-            <div
-              className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all'>
-              <h3 className='text-xl font-bold mb-2'>
                 {t('ecommerce_title')}
               </h3>
               <p className='text-gray-400 mb-4'>
@@ -147,43 +150,6 @@ export const Projects = () => {
                     'JavaScript',
                     'React',
                     'Node.js'
-                  ].map((tech, key) => (
-                    <span
-                      key={key}
-                      className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition'
-                    >
-                      {tech}
-                    </span>
-                  ))}
-              </div>
-
-              <div className='flex justify-between items-center'>
-                <a
-                  href='#'
-                  className='text-blue-400 hover:text-blue-300 transition-colors my-4'
-                >
-                  {t('view_project')} &#8594;
-                </a>
-              </div>
-            </div>
-
-            <div
-              className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all'>
-              <h3 className='text-xl font-bold mb-2'>
-                {t('cryptocurrency_tracker')}
-              </h3>
-              <p className='text-gray-400 mb-4'>
-                {t('cryptocurrency_tracker')}
-              </p>
-              <div className='flex flex-wrap gap-2 mb-4'>
-                {
-                  [
-                    'HTML',
-                    'CSS',
-                    'JavaScript',
-                    'React',
-                    'Recharts',
-                    'Coingecko API'
                   ].map((tech, key) => (
                     <span
                       key={key}
